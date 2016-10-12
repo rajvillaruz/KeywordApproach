df74746d410cceb46637acd68efe16b07e45513d
rajvillaruz <rochellejuanvillaruz@gmail.com>
2016-10-12 11:39:02 +0800

    Add Keyword basic Functions

--- src/com/ka/libraries/Keyword.java
+++ src/com/ka/libraries/Keyword.java
@@ -1,5 +1,28 @@
 package com.ka.libraries;
 
 public class Keyword {
-
+	
+	public static void openBrowser() {
+		
+	}
+	
+	public static void launchApp() {
+		
+	}
+	
+	public static void inputText() {
+		
+	}
+	
+	public static void chooseSelect() {
+		
+	}
+	
+	public static void chooseRadio() {
+		
+	}
+	
+	public static void chooseCheckBox() {
+		
+	}
 }
